e97e2e1656b8bc1c58213165c4e8b58710e141d8
Michael Van Kirk <vankirk.6@wright.edu>
2022-08-07 11:55:05 -0400

    added SDL2 support

--- source-files/main.cpp
+++ source-files/main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 #include "../header-files/Bus.h"
+#include "SDL2/SDL.h"
+
+#define SCREEN_HEIGHT 600
+#define SCREEN_WIDTH 800
 
 int main() {
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window = SDL_CreateWindow("NES Emulator", 0, 0,
+                                          SCREEN_HEIGHT, SCREEN_WIDTH, SDL_WINDOW_HIDDEN);
+    SDL_ShowWindow(window);
+    SDL_Event event;
+    int running = 1;
+    while(running) {
+        while(SDL_PollEvent(&event)) {
+            if(event.type == SDL_QUIT) {
+                running = 0;
+            }
+        }
+        SDL_Delay( 32 );
+    }
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+
+
     std::cout << "Hello, World!" << std::endl;
     Bus bus;
     bus.write(0x0000, 48);
